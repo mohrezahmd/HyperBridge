93017493961d8ee1c0b1ec98afdf1e27357616ce
Mohammad Reza Ahmadi <mohrezahmd@gmail.com>
2024-08-31 20:36:24 +0330

    making items fit in screen

--- Assets/Scripts/FitWorkgroundToCamera.cs
+++ Assets/Scripts/FitWorkgroundToCamera.cs
@@ -9,8 +9,8 @@ public class FitWorkgroundToCamera : MonoBehaviour
         mainCamera = Camera.main;
         transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, 0);
 
-        Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(Vector3.zero) * 100;
-        Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(mainCamera.rect.width, mainCamera.rect.height)) * 100;
+        Vector3 bottomLeft = mainCamera.ViewportToWorldPoint(Vector3.zero);// * 100;
+        Vector3 topRight = mainCamera.ViewportToWorldPoint(new Vector3(mainCamera.rect.width, mainCamera.rect.height));// * 100;
         Vector3 screenSize = topRight - bottomLeft;
         float screenRatio = screenSize.x / screenSize.y;
         float desiredRatio = transform.localScale.x / transform.localScale.y;
